9ad69588fc032e9c9710b0913c7e03f6ab7f4db2
JohanKovamees <johan@kova-tech.com>
2022-04-11 00:22:38 +0200

    Fixed some file troubles

--- duplicate_remover.py
+++ duplicate_remover.py
@@ -1,8 +1,10 @@
 #!usr/bin/env python3
 #Script that compares all .jpg & .png in a folder and removes duplicate files
 
+#Suggestion for improvement: Base delete on numpy objects that are equal instead of comparing the filesize
+
 import os
-import numpy as np
+#import numpy as np
 
 path_to_folder = input("Enter the path to the folder: ")
 
@@ -14,5 +16,3 @@ def duplicate_remover(path_to_folder):
         if filename.endswith(".jpg") or filename.endswith(".png"):
             if os.path.getsize(os.path.join(path_to_folder, filename)) in os.listdir(path_to_folder):
                 os.remove(os.path.join(path_to_folder, filename))
-        else:
-            os.remove(os.path.join(path_to_folder, filename))
